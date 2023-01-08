3ec974aeec0df78a1120855e40e029b07480bd3f
KhoirulAnam78 <khoirulanam4580@gmail.com>
2023-01-08 08:06:33 +0700

    Crud data siswa

---
+++ app/Http/Livewire/TabelSiswa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TabelSiswa extends Component
+{
+    public function render()
+    {
+        return view('livewire.tabel-siswa');
+    }
+}
