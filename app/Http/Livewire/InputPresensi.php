c55a4067bbb8d371b358f0bb84060fe612f60e45
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2023-02-08 12:50:06 +0700

    Input presensi

--- app/Http/Livewire/InputPresensi.php
+++ app/Http/Livewire/InputPresensi.php
@@ -2,27 +2,111 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\JadwalPelajaran;
-use App\Models\TahunAkademik;
+use App\Models\JadwalGuruPiket;
+use App\Models\Kelas;
 use Livewire\Component;
+use App\Models\TahunAkademik;
+use App\Models\JadwalPelajaran;
+use App\Models\MonitoringPembelajaran;
 
 class InputPresensi extends Component
 {
     public $filterKelas = '';
+    public $siswa;
     public $day;
     public $mapel;
-
+    public $tanggal, $waktu_mulai, $waktu_berakhir, $topik;
+    public $jadwal_pelajaran_id;
+    public $presensi = [];
     public function mount()
     {
-        $this->day = \Carbon\Carbon::now()->translatedFormat('l');
+        $this->filterKelas = TahunAkademik::where('status', 'aktif')->first()->kelas->first()->id;
+        $this->siswa = Kelas::where('id', $this->filterKelas)->first()->siswas->all();
+        foreach ($this->siswa as $s) {
+            $this->presensi[$s->id] = 'hadir';
+        }
+    }
+
+    public function rules()
+    {
+        return [
+            'tanggal' => 'required|date',
+            'topik' => 'required',
+            'waktu_mulai' => 'required|date_format:H:i',
+            'waktu_berakhir' => 'required|date_format:H:i|after:waktu_mulai',
+        ];
+    }
+
+    //Custom Errror messages for validation
+    protected $messages = [
+        'tanggal.required' => 'Tanggal wajib diisi !',
+        'waktu_mulai.required' => 'Waktu mulai wajib diisi !',
+        'waktu_mulai.date_format' => 'Waktu mulai hanya diperbolehkan format waktu !',
+        'waktu_berakhir.required' => 'Waktu berakhir wajib diisi !',
+        'waktu_berakhir.date_format' => 'Waktu berakhir hanya diperbolehkan format waktu !',
+        'waktu_berakhir.after' => 'Waktu berakhir harus lebih besar dari waktu mulai !',
+        'topik.required' => 'Topik/Agenda wajib diisi !',
+    ];
+
+    public function empty()
+    {
+        $this->topik = null;
+        $this->waktu_mulai = null;
+        $this->waktu_berakhir = null;
+        $this->tanggal = null;
+        $this->resetErrorBag();
+        $this->resetValidation();
+        // $this->dispatchBrowserEvent('close-input-modal');
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
     }
 
     public function updatedFilterKelas()
     {
-        $this->mapel = JadwalPelajaran::with('guru')->with('mataPelajaran')->where('kelas_id', $this->filterKelas)->where('hari', $this->day)->get()->all();
+        $this->mapel = JadwalPelajaran::with('guru')->with('kelas')->with('mataPelajaran')->where('kelas_id', 'like', '%' . $this->filterKelas . '%')->where('hari', $this->day)->get()->all();
+        $this->siswa = Kelas::where('id', $this->filterKelas)->first()->siswas->all();
     }
+
+    public function save()
+    {
+        dd('Belum bisa di save wkwk');
+        foreach ($this->presensi as $key => $value) {
+            dd($key);
+        }
+        $this->validate();
+        $jadwalToday = JadwalGuruPiket::where('hari', \Carbon\Carbon::now()->translatedFormat('l'))->where(function ($query) {
+            $query->where('waktu_mulai', '<=', \Carbon\Carbon::now()->translatedFormat('h:i'))->orWhere('waktu_berakhir', '>=', \Carbon\Carbon::now()->translatedFormat('h:i'));
+        })->get();
+        $guruPiketId = $jadwalToday->guru_id;
+        $monitoring = MonitoringPembelajaran::create([
+            'tanggal' => $this->tanggal,
+            'topik' => $this->topik,
+            'waktu_mulai' => $this->waktu_mulai,
+            'waktu_berakhir' => $this->waktu_berakhir,
+            'status_validasi' => 'tidak valid',
+            'jadwal_pelajaran_id' => $this->jadwal_pelajaran_id,
+            'guru_piket_id' => $guruPiketId
+        ]);
+
+        session()->flash('message', 'Data berhasil ditambahkan !');
+        $this->empty();
+        $this->dispatchBrowserEvent('close-modal');
+    }
+
+    public function inputPresensi($id)
+    {
+        $this->jadwal_pelajaran_id = $id;
+        $this->dispatchBrowserEvent('show-input-modal');
+    }
+
     public function render()
     {
+        $this->siswa = Kelas::where('id', $this->filterKelas)->first()->siswas->all();
+        $this->day = \Carbon\Carbon::now()->translatedFormat('l');
+        $this->mapel = JadwalPelajaran::with('guru')->with('kelas')->with('mataPelajaran')->where('kelas_id', 'like', '%' . $this->filterKelas . '%')->where('hari', $this->day)->get()->all();
         return view('livewire.input-presensi', [
             'kelas' => TahunAkademik::where('status', 'aktif')->first()->kelas,
             'mapel' => $this->mapel
--- resources/views/livewire/input-presensi.blade.php
+++ resources/views/livewire/input-presensi.blade.php
@@ -19,32 +19,37 @@
         <label for="kelas_id" class="form-label">Kelas</label>
         <select wire:model="filterKelas" id="kelas_id" class="form-select">
             @if ($kelas !== null)
-                <option value="">Pilih</option>
                 @foreach ($kelas as $k)
                     <option value="{{ $k->id }}">{{ $k->nama }}</option>
                 @endforeach
             @else
-                <option>Pilih</option>
+                <option>Tidak ada kelas</option>
             @endif
         </select>
     </div>
     <div class="row mx-2 mb-3">
         @if ($mapel !== null)
             @foreach ($mapel as $m)
-                <div class="col-md-4 col-xl-4">
-                    <a href="" class="btn btn-primary">
-                        <div class="card">
-                            <div class="card-body">
-                                <h6 class="card-title">Guru : {{ $m->guru->nama }}</h6>
-                                <h5 class="card-title">{{ $m->mataPelajaran->nama }}</h5>
-                                <h6 class="card-title">Jam pelajaran
-                                    {{ substr($m->waktu_mulai, 0, -3) . '-' . substr($m->waktu_berakhir, 0, -3) }}</h6>
-                            </div>
-                        </div>
-                    </a>
+                <div class="col-lg-4 col-md-4 col-xl-4">
+                    <button data-bs-toggle="modal" data-bs-target="#inputModal"
+                        wire:click="inputPresensi({{ $m->id }})" class="btn btn-primary">
+                        <h5 class="card-title text-white">{{ $m->kelas->nama }}</h5>
+                        <h6 class="card-title text-white">Guru : {{ $m->guru->nama }}</h6>
+                        <h5 class="card-title text-white">{{ $m->mataPelajaran->nama }}</h5>
+                        <h6 class="card-title text-white">Jam pelajaran
+                            {{ substr($m->waktu_mulai, 0, -3) . '-' . substr($m->waktu_berakhir, 0, -3) }}</h6>
+                    </button>
                 </div>
             @endforeach
         @endif
     </div>
-
+    @include('livewire.modals.modal-input-presensi')
+    <script>
+        window.addEventListener('close-input-modal', event => {
+            $('#InputModal').modal('hide')
+        })
+        window.addEventListener('show-input-modal', event => {
+            $('#inputModal').modal('show');
+        });
+    </script>
 </div>
