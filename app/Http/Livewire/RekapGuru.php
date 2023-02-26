6d3eb3a794b315b4c36dd4c18ad5fbb2c64f8af0
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2023-02-26 07:56:29 +0700

    Update rekap guru

--- app/Http/Livewire/RekapGuru.php
+++ app/Http/Livewire/RekapGuru.php
@@ -26,10 +26,19 @@ class RekapGuru extends Component
     }
     public function export()
     {
-        return Excel::download(new RekapGuruExport($this->tanggalAwal, $this->tanggalAkhir, $this->kelasAktif), 'Rekap Guru ' . 'Tanggal ' . $this->tanggalAwal . ' Sampai ' . $this->tanggalAkhir . '.xlsx');
+        return Excel::download(new RekapGuruExport($this->kelasAktif, $this->tanggalAwal, $this->tanggalAkhir), 'Rekap Guru ' . 'Tanggal ' . $this->tanggalAwal . ' Sampai ' . $this->tanggalAkhir . '.xlsx');
     }
     public function render()
     {
+        // $data = Guru::select('id', 'nama', 'kode_guru')->where('nama', 'like', '%' . $this->search . '%')->with(['jadwalPelajarans' => function ($query) {
+        //     $query->select('id', 'waktu_mulai', 'waktu_berakhir', 'guru_id', 'mata_pelajaran_id')->whereIn('kelas_id', $this->kelasAktif)->with(['monitoringPembelajarans' => function ($query) {
+        //         $query->select('id', 'status_validasi', 'jadwal_pelajaran_id', 'waktu_mulai', 'waktu_berakhir')->where('tanggal', '>=', $this->tanggalAwal)->where('tanggal', '<=', $this->tanggalAkhir);
+        //     }])->with(['mataPelajaran' => function ($query) {
+        //         $query->select('id', 'nama');
+        //     }]);
+        // }])->paginate(10);
+
+        // dd($data);
         return view('livewire.rekap-guru', [
             'guru' => Guru::select('id', 'nama', 'kode_guru')->where('nama', 'like', '%' . $this->search . '%')->with(['jadwalPelajarans' => function ($query) {
                 $query->select('id', 'waktu_mulai', 'waktu_berakhir', 'guru_id', 'mata_pelajaran_id')->whereIn('kelas_id', $this->kelasAktif)->with(['monitoringPembelajarans' => function ($query) {
---
+++ resources/views/livewire/tables/table-rekap-guru.blade.php
@@ -0,0 +1,136 @@
+<table class="table table-striped align-top" id="example">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Kode</th>
+            <th>Mata Pelajaran</th>
+            <th>Jam Wajib</th>
+            <th>Tidak Terlaksana</th>
+            <th>% Terlaksana</th>
+            <th>Keterangan</th>
+        </tr>
+    </thead>
+    {{-- @php
+          dd($guru);
+      @endphp --}}
+    <tbody class="table-border-bottom-0">
+        @if (count($guru) === 0)
+            <tr>
+                <td colspan='9' align="center"><span>Tidak ada data</span></td>
+            </tr>
+        @else
+            @foreach ($guru as $g)
+                @php
+                    $b = $g->jadwalPelajarans->groupBy('mata_pelajaran_id')->first();
+                    $rowCount = count($g->jadwalPelajarans->groupBy('mata_pelajaran_id'));
+                @endphp
+                <tr>
+                    <td
+                        {{ count($g->jadwalPelajarans->groupBy('mata_pelajaran_id')) !== 1 ? 'rowspan=' . $rowCount : '' }}>
+                        {{ ($guru->currentpage() - 1) * $guru->perpage() + $loop->index + 1 }}
+                    </td>
+                    <td
+                        {{ count($g->jadwalPelajarans->groupBy('mata_pelajaran_id')) !== 1 ? 'rowspan=' . $rowCount : '' }}>
+                        {{ $g->nama }}</td>
+                    <td
+                        {{ count($g->jadwalPelajarans->groupBy('mata_pelajaran_id')) !== 1 ? 'rowspan=' . $rowCount : '' }}>
+                        {{ $g->kode_guru }}</td>
+                    <td>{{ $b->first()->mataPelajaran->nama }}</td>
+                    @php
+                        $diff = new DateTime('00:00');
+                        foreach ($b as $j) {
+                            $date1 = new DateTime(substr($j->waktu_mulai, 0, -3));
+                            $date2 = new DateTime(substr($j->waktu_berakhir, 0, -3));
+                            $diff = $diff->sub($date2->diff($date1));
+                        }
+                    @endphp
+                    <td align="center">{{ $diff->format('g.i') }}</td>
+
+                    @php
+                        foreach ($b as $j) {
+                            $jml = new DateTime('00:00');
+                            if (count($j->monitoringPembelajarans) !== 0) {
+                                foreach ($j->monitoringPembelajarans as $m) {
+                                    if ($m->status_validasi === 'tidak valid') {
+                                        $date1 = new DateTime(substr($m->waktu_mulai, 0, -3));
+                                        $date2 = new DateTime(substr($m->waktu_berakhir, 0, -3));
+                                        $jml = $jml->sub($date2->diff($date1));
+                                    }
+                                }
+                            }
+                        }
+                    @endphp
+                    <td align="center">{{ $jml->format('g.i') === '12.00' ? '0' : $jml->format('g.i') }}
+                    </td>
+                    <td align="center">
+                        @if ($jml->format('g.i') === '12.00')
+                            {{ '100%' }}
+                        @else
+                            @php
+                                $data1 = $jml->format('g.i');
+                                $data2 = $diff->format('g.i');
+                                $data1int = (int) $data1;
+                                $data2int = (int) $data2;
+                                $total = (($data2int - $data1int) / $data2int) * 100;
+                            @endphp
+                            {{ $total . '%' }}
+                        @endif
+                    </td>
+                    <td></td>
+                </tr>
+                @foreach ($g->jadwalPelajarans->groupBy('mata_pelajaran_id') as $key => $b)
+                    @if ($loop->first)
+                        @continue
+                    @endif
+                    <tr>
+                        <td>{{ $b->first()->mataPelajaran->nama }}</td>
+                        @php
+                            $diff = new DateTime('00:00');
+                            foreach ($b as $j) {
+                                $date1 = new DateTime(substr($j->waktu_mulai, 0, -3));
+                                $date2 = new DateTime(substr($j->waktu_berakhir, 0, -3));
+                                $diff = $diff->sub($date2->diff($date1));
+                            }
+                        @endphp
+                        <td align="center">{{ $diff->format('g.i') }}</td>
+
+                        @php
+                            foreach ($b as $j) {
+                                $jml = new DateTime('00:00');
+                                if (count($j->monitoringPembelajarans) !== 0) {
+                                    foreach ($j->monitoringPembelajarans as $m) {
+                                        if ($m->status_validasi === 'tidak valid') {
+                                            $date1 = new DateTime(substr($m->waktu_mulai, 0, -3));
+                                            $date2 = new DateTime(substr($m->waktu_berakhir, 0, -3));
+                                            $jml = $jml->sub($date2->diff($date1));
+                                        }
+                                    }
+                                }
+                            }
+                        @endphp
+                        <td align="center">
+                            {{ $jml->format('g.i') === '12.00' ? '0' : $jml->format('g.i') }}</td>
+                        <td align="center">
+                            @if ($jml->format('g.i') === '12.00')
+                                {{ '100%' }}
+                            @else
+                                @php
+                                    $data1 = $jml->format('g.i');
+                                    $data2 = $diff->format('g.i');
+                                    $data1int = (int) $data1;
+                                    $data2int = (int) $data2;
+                                    $total = (($data2int - $data1int) / $data2int) * 100;
+                                @endphp
+                                {{ $total . '%' }}
+                            @endif
+                        </td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            @endforeach
+        @endif
+
+    </tbody>
+
+</table>
