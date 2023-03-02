2cf0ed98264b98aa0987a94bfd44fcf6549e839e
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2023-03-02 23:12:57 +0700

    Update api login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Guru;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\MataPelajaran;
+use App\Models\TahunAkademik;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
 {
+    public $kelasAktif = [];
     public function login(Request $request)
     {
         if (!Auth::attempt($request->only('username', 'password'))) {
@@ -21,19 +24,40 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
         // $data = PersonalAccessToken::findToken($token)->tokenable();
         if ($user->role === 'admin') {
-            $dataUser = auth('sanctum')->user();
+            return response()->json([
+                'message' => 'Login success',
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'role' => auth('sanctum')->user()->role,
+            ]);
         } else if ($user->role === 'guru') {
-            $dataUser = $user->guru;
+            $data = TahunAkademik::select('id')->where('status', 'aktif')->first()->kelas->all();
+            foreach ($data as $d) {
+                array_push($this->kelasAktif, $d->id);
+            }
+            $dataUser = Guru::where('user_id', auth('sanctum')->user()->id)->select('id', 'nama')->with(['jadwalPelajarans' => function ($query) {
+                $query->select('id', 'guru_id', 'mata_pelajaran_id')->whereIn('kelas_id', $this->kelasAktif)->with(['mataPelajaran' => function ($query) {
+                    $query->select('id', 'nama');
+                }]);
+            }])->first();
+            $user = ['nama' => $dataUser->nama];
+            $mapel = [];
+            $dataMapel = $dataUser->jadwalPelajarans->groupBy('mata_pelajaran_id');
+            foreach ($dataMapel as $key => $dm) {
+                $mataPelajaran = MataPelajaran::select('nama')->where('id', $key)->first();
+                array_push($mapel, $mataPelajaran->nama);
+            }
+            return response()->json([
+                'message' => 'Login success',
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'role' => auth('sanctum')->user()->role,
+                'user' => $user,
+                'mapel' => $mapel
+            ]);
         } else {
             $dataUser = $user->siswa;
         }
-        return response()->json([
-            'message' => 'Login success',
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-            'role' => auth('sanctum')->user()->role,
-            'user' => $dataUser
-        ]);
     }
 
     public function logout(Request $request)
