14b212c5500e65c6937ac5dcedf9ee7d1647d65d
KhoirulAnam78 <khoirulanam4580@gmail.com>
2023-03-18 12:03:58 +0700

    update image carrousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="container">
-        <img src="{{ url('') }}/assets/assets/img/titers.webp" class="img-fluid" alt="...">
+        <img src="{{ url('') }}/assets/assets/img/titers.webp" class="img-fluid" alt="carrousel-image.jpg">
     </div>
 @endsection
