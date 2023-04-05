3dbd08a4ce43997ce5cce11e262f0fca08078f40
KhoirulAnam78 <khoirulanam4580@gmail.com>
2023-04-05 09:36:05 +0700

    (End Increment 1) perbaikan file migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
-            $table->enum('role', ['admin', 'pimpinan', 'guru', 'waliAsrama', 'siswa']);
+            $table->enum('role', ['admin', 'pimpinan', 'guru', 'wali_asrama', 'siswa']);
             // $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- database/migrations/2022_12_19_043116_create_gurus_table.php
+++ database/migrations/2022_12_19_043116_create_gurus_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('nama', 50);
             $table->char('no_telp', 14);
             $table->enum('status', ['aktif', 'tidak aktif']);
+            $table->boolean('pimpinan')->default(false);
             $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
