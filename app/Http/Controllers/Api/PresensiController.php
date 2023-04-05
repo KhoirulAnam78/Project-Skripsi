6e487f51b2c14d1d1aebd01354665b4b3a1e2b7d
KhoirulAnam78 <khoirulanam4580@gmail.com>
2023-04-06 00:50:08 +0700

    tes11

--- app/Http/Controllers/Api/PresensiController.php
+++ app/Http/Controllers/Api/PresensiController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Models\Kelas;
 use App\Models\Siswa;
 use Illuminate\Http\Request;
+use App\Models\JadwalGuruPiket;
 use App\Http\Controllers\Controller;
+use App\Models\KehadiranPembelajaran;
+use App\Models\MonitoringPembelajaran;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx\Rels;
 
 class PresensiController extends Controller
@@ -29,17 +32,44 @@ class PresensiController extends Controller
 
     public function presensiPembelajaran(Request $request)
     {
-        // return $request->presensi;
-        $data = json_decode($request->presensi);
-        // // return $data[0]->id;
-
+        $presensi = json_decode($request->presensi);
+        // if (auth('sanctum')->user()->role === 'guru') {
+        //     $jadwalToday = JadwalGuruPiket::where('hari', \Carbon\Carbon::now()->translatedFormat('l'))->where(function ($query) {
+        //         $query->where('waktu_mulai', '<=', \Carbon\Carbon::now()->translatedFormat('h:i'))->orWhere('waktu_berakhir', '>=', \Carbon\Carbon::now()->translatedFormat('h:i'));
+        //     })->first();
+        //     $status = 'belum tervalidasi';
+        //     if ($jadwalToday === null) {
+        //         $guruPiketId = null;
+        //     } else {
+        //         $guruPiketId = $jadwalToday->guru_id;
+        //     }
+        // } else {
+        //     $guruPiketId = null;
+        //     $status = 'valid';
+        // }
+        // $monitoring = MonitoringPembelajaran::create([
+        //     'tanggal' => $request->tanggal,
+        //     'topik' => $request->agendaBelajar,
+        //     'waktu_mulai' => $request->jamDimulai,
+        //     'waktu_berakhir' => $request->jamBerakhir,
+        //     'status_validasi' => $status,
+        //     'jadwal_pelajaran_id' => $request->filterMapel,
+        //     'guru_piket_id' => $guruPiketId
+        // ]);
+        // foreach ($presensi as $value) {
+        //     KehadiranPembelajaran::create([
+        //         'siswa_id' => $value->siswaID,
+        //         'status' => $value->status,
+        //         'monitoring_pembelajaran_id' => $monitoring->id
+        //     ]);
+        // }
         return response()->json([
             'message' => 'Data berhasil dikirim',
             'tanggal' => $request->tanggal,
             'waktu_mulai' => $request->jamDimulai,
             'waktu_berakhir' => $request->jamBerakhir,
             'topik' => $request->agendaBelajar,
-            'presensi' => $data[0],
+            'presensi' => $presensi[0]->siswaID,
             'jadwal_id' => $request->jadwalId
         ]);
     }
