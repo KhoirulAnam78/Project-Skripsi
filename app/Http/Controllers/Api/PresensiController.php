15244311a8bb4584969690168820a6557ac3812b
KhoirulAnam78 <khoirulanam4580@gmail.com>
2023-04-06 00:38:04 +0700

    tes9

--- app/Http/Controllers/Api/PresensiController.php
+++ app/Http/Controllers/Api/PresensiController.php
@@ -39,7 +39,7 @@ class PresensiController extends Controller
             'waktu_mulai' => $request->jamDimulai,
             'waktu_berakhir' => $request->jamBerakhir,
             'topik' => $request->agendaBelajar,
-            'presensi' => $request->presensi,
+            'presensi' => json_decode($request->presensi),
             'jadwal_id' => $request->jadwalId
         ]);
     }
