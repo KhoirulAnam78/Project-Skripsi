4cb6424e798ca6a8b829df749bd4486ef2b1c63f
KhoirulAnam78 <khoirulanam4580@gmail.com>
2023-04-11 22:53:45 +0700

    send jadwal pengganti

--- app/Http/Controllers/Api/GuruApiController.php
+++ app/Http/Controllers/Api/GuruApiController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\TahunAkademik;
 use App\Models\JadwalPelajaran;
+use App\Models\JadwalPengganti;
 use App\Http\Controllers\Controller;
 
 class GuruApiController extends Controller
@@ -14,7 +15,7 @@ class GuruApiController extends Controller
 
   public function getJadwal(Request $request)
   {
-    if ($request->hari) {
+    if ($request->hari && $request->tanggal) {
       $data = TahunAkademik::select('id')->where('status', 'aktif')->first()->kelas->all();
       foreach ($data as $d) {
         array_push($this->kelasAktif, $d->id);
@@ -24,14 +25,26 @@ class GuruApiController extends Controller
       }])->with(['mataPelajaran' => function ($query) {
         $query->select('id', 'nama');
       }])->select('id', 'hari', 'waktu_mulai', 'waktu_berakhir', 'kelas_id', 'mata_pelajaran_id')->get();
+
+      $jadwalPengganti = JadwalPengganti::where('tanggal', $request->tanggal)->whereRelation('jadwalPelajaran', 'guru_id', auth('sanctum')->user()->guru->id)->with(['jadwalPelajaran' => function ($query) {
+        // $query->with()->get();
+        $query->with(['kelas' => function ($query) {
+          $query->select('id', 'nama');
+        }])->with(['mataPelajaran' => function ($query) {
+          $query->select('id', 'nama');
+        }])->get();
+      }])->get();
+
       return response()->json([
         'message' => 'Fetch data success',
+        'tanggal' => $request->tanggal,
         'jadwal-mengajar' => $jadwal,
+        'jadwal-pengganti' => $jadwalPengganti
       ]);
     } else {
       return response()->json([
         'message' => 'Fetch data failed',
-        'request' => 'Hari is required !',
+        'request' => 'Hari and tanggal is required !',
       ]);
     }
   }
