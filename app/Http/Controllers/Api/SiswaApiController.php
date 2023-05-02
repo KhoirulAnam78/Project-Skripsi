1c39f9bb5224e510291568f01c84d6dab2e8c277
KhoirulAnam78 <khoirulanam4580@gmail.com>
2023-05-03 00:05:14 +0700

    send jadwal pengganti

--- app/Http/Controllers/Api/SiswaApiController.php
+++ app/Http/Controllers/Api/SiswaApiController.php
@@ -5,25 +5,39 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Siswa;
 use Illuminate\Http\Request;
+use App\Models\JadwalPengganti;
 use App\Http\Controllers\Controller;
 
 class SiswaApiController extends Controller
 {
+  public $jadwal;
   public function getJadwal(Request $request)
   {
     if ($request->hari) {
-      $jadwal = Siswa::where('user_id', auth('sanctum')->user()->id)->select('id', 'user_id')->with(['kelas' => function ($query) {
+      $this->jadwal = Siswa::where('user_id', auth('sanctum')->user()->id)->select('id', 'user_id')->with(['kelas' => function ($query) {
         $query->whereRelation('tahunAkademik', 'status', 'aktif')->with('jadwalPelajarans');
       }])->first();
+
+      $jadwalPengganti = JadwalPengganti::where('tanggal', $request->tanggal)->with(['jadwalPelajaran' => function ($query) {
+        // $query->with()->get();
+        $query->where('kelas_id', $this->jadwal->kelas->id)->with(['kelas' => function ($query) {
+          $query->select('id', 'nama');
+        }])->with(['mataPelajaran' => function ($query) {
+          $query->select('id', 'nama');
+        }])->get();
+      }])->get();
+
       return response()->json([
         'message' => 'Fetch data success',
-        'jadwal-siswa' => $jadwal->kelas->first()->jadwalPelajarans()->where('hari', $request->hari)->with('mataPelajaran', 'guru')->with(['monitoringPembelajarans' => function ($query) {
+        'jadwal-siswa' => $this->jadwal->kelas->first()->jadwalPelajarans()->where('hari', $request->hari)->with('mataPelajaran', 'guru')->with(['monitoringPembelajarans' => function ($query) {
           if ($query) {
             $query->with(['kehadiranPembelajarans' => function ($query) {
               $query->where('siswa_id', auth('sanctum')->user()->siswa->id);
             }]);
           };
         }])->get(),
+
+        'jadwal-pengganti' => $jadwalPengganti
       ]);
     } else {
       return response()->json([
