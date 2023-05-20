25f29a19cb119db61fa6d5555f1639e57395dc75
Khoirul Anam <khoirulanam4580@gmail.com>
2023-05-20 21:03:30 +0700

    update rekap export

--- app/Exports/RekapGuruExport.php
+++ app/Exports/RekapGuruExport.php
@@ -21,7 +21,7 @@ class RekapGuruExport implements FromView
         if (Auth::user()->role === 'guru') {
             $data = Guru::select('id', 'nama', 'kode_guru')->where('id', Auth::user()->guru->id)->with(['jadwalPelajarans' => function ($query) {
                 $query->select('id', 'waktu_mulai', 'waktu_berakhir', 'guru_id', 'mata_pelajaran_id')->whereIn('kelas_id', $this->kelasAktif)->with(['monitoringPembelajarans' => function ($query) {
-                    $query->select('id', 'status_validasi', 'jadwal_pelajaran_id', 'waktu_mulai', 'waktu_berakhir', 'keterangan')->where('tanggal', '>=', $this->tanggalAwal)->where('tanggal', '<=', $this->tanggalAkhir);
+                    $query->select('id', 'tanggal', 'status_validasi', 'jadwal_pelajaran_id', 'waktu_mulai', 'waktu_berakhir', 'keterangan')->where('tanggal', '>=', $this->tanggalAwal)->where('tanggal', '<=', $this->tanggalAkhir);
                 }])->with(['mataPelajaran' => function ($query) {
                     $query->select('id', 'nama');
                 }]);
@@ -29,7 +29,7 @@ class RekapGuruExport implements FromView
         } else {
             $data = Guru::select('id', 'nama', 'kode_guru')->with(['jadwalPelajarans' => function ($query) {
                 $query->select('id', 'waktu_mulai', 'waktu_berakhir', 'guru_id', 'mata_pelajaran_id')->whereIn('kelas_id', $this->kelasAktif)->with(['monitoringPembelajarans' => function ($query) {
-                    $query->select('id', 'status_validasi', 'jadwal_pelajaran_id', 'waktu_mulai', 'waktu_berakhir', 'keterangan')->where('tanggal', '>=', $this->tanggalAwal)->where('tanggal', '<=', $this->tanggalAkhir);
+                    $query->select('id', 'tanggal', 'status_validasi', 'jadwal_pelajaran_id', 'waktu_mulai', 'waktu_berakhir', 'keterangan')->where('tanggal', '>=', $this->tanggalAwal)->where('tanggal', '<=', $this->tanggalAkhir);
                 }])->with(['mataPelajaran' => function ($query) {
                     $query->select('id', 'nama');
                 }]);
