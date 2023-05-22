898c235f660df15bc9e570fc2d863e189099b639
Khoirul Anam <khoirulanam4580@gmail.com>
2023-05-22 07:56:00 +0700

    test 2

--- resources/views/livewire/validasi-pembelajaran.blade.php
+++ resources/views/livewire/validasi-pembelajaran.blade.php
@@ -86,6 +86,7 @@
                                 <td>{{ substr($j->waktu_mulai, 0, -3) . '-' . substr($j->waktu_berakhir, 0, -3) }}</td>
                                 @php
                                     $data = $j->jadwalPelajaran->monitoringPembelajarans->where('tanggal', \Carbon\Carbon::now()->translatedFormat('Y-m-d'))->first();
+                                    dd($data);
                                 @endphp
                                 <td>{{ count($data) === 0 ? '' : $data->topik }}
                                 </td>
