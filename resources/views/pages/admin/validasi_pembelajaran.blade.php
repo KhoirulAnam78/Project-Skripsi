fa9cc4823d6bd854995325e1399347966c87784b
Khoirul Anam <khoirulanam4580@gmail.com>
2023-05-27 23:41:11 +0700

    update header halaman

--- resources/views/pages/admin/validasi_pembelajaran.blade.php
+++ resources/views/pages/admin/validasi_pembelajaran.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.main')
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Validasi Pembelajaran</span></h4>
+        <h4 class="fw-bold py-3 mb-4">Validasi Pembelajaran</h4>
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
