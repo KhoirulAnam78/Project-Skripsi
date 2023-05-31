d4f79acd230eb24b59e32b7a91139c834e56c543
Khoirul Anam <khoirulanam4580@gmail.com>
2023-06-01 02:40:36 +0700

    update send auth wali asrama

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -83,7 +83,8 @@ class AuthController extends Controller
                 'token_type' => 'Bearer',
                 'role' => auth('sanctum')->user()->role,
                 'nama' => $user->waliAsrama->nama,
-                'angkatan' => ['id' => $angkatan->id, 'nama' => $angkatan->nama]
+                'angkatan_id' => $angkatan->id,
+                'angkatan' => $angkatan->nama
             ]);
         } else {
             return response()->json([
