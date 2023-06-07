ac37ac808c6cf5f62d24f6fbb796646b3c34a16f
Khoirul Anam <khoirulanam4580@gmail.com>
2023-06-08 01:44:19 +0700

    test 2

--- app/Http/Controllers/Api/PimpinanApiController.php
+++ app/Http/Controllers/Api/PimpinanApiController.php
@@ -296,7 +296,7 @@ class PimpinanApiController extends Controller
       $angkatan_id = Kelas::find($request->kelas_id)->angkatan->id;
 
       $jadwal = JadwalKegiatan::where('kegiatan_id', $request->kegiatan_id)
-        ->where('hari', '=', 'Setiap Hari')->orwhere('hari', '=', $request->hari)
+        ->where('hari', '=', $request->hari)
         ->where('angkatan_id', $angkatan_id)->where('tahun_akademik_id', $this->tahunAkademik)->with('kegiatan')
         ->with(['monitoringKegnas' => function ($query) {
           if ($query) {
