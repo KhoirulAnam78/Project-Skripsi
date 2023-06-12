c93afebacef117683378bd145124c2f8ee2f98ba
Khoirul Anam <khoirulanam4580@gmail.com>
2023-06-12 10:24:04 +0700

    update tampilan tidak ada data

--- resources/views/livewire/pembelajaran-monitoring.blade.php
+++ resources/views/livewire/pembelajaran-monitoring.blade.php
@@ -139,7 +139,6 @@
         </div>
 
         <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6">
-
             <div class="card bg-secondary text-white mb-3">
                 <div class="card-header fw-bold">Siswa Tidak Hadir Dalam Pembelajaran</div>
                 <div class="card-body">
@@ -160,6 +159,11 @@
                                 @endforeach
                             </p>
                         @endforeach
+                    @else
+                        <p class="card-text fw-bold">
+                            --Tidak ada data--
+
+                        </p>
                     @endif
                 </div>
             </div>
