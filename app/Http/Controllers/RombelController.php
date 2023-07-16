9e923a1c05affcb5ab97bb098f01178d120cce12
Khoirul Anam <khoirulanam4580@gmail.com>
2023-07-16 21:52:30 +0700

    update link download template rombel

--- app/Http/Controllers/RombelController.php
+++ app/Http/Controllers/RombelController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Rombel;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\StoreRombelRequest;
-use App\Http\Requests\UpdateRombelRequest;
+use Illuminate\Support\Facades\Response;
 
 class RombelController extends Controller
 {
@@ -23,5 +22,19 @@ class RombelController extends Controller
         return view('pages.admin.rombel', [
             'title' => 'Rombongan Belajar'
         ]);
+    }
+
+
+    public function download()
+    {
+        if (Auth::user()->role === 'siswa') {
+            return abort(403, 'Anda tidak memiliki akses kehalaman ini.');
+        }
+        $file = public_path() . '/assets/template-excel/Data Siswa.xlsx';
+        $headers = array(
+            'Content-Type: application/xlsx',
+        );
+
+        return Response::download($file, 'Template Import Data Rombel.xlsx', $headers);
     }
 }
